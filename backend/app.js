edfda30c406c77ddbf5af6e2d2f5dc25524a12a0
Arthur_Sauvage <77831535+Arthur-sauvage@users.noreply.github.com>
2022-06-14 20:24:53 +0200

    fix

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,7 @@ const { Records } = require("./models/records");
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: process.env.NODE_ENV + ":" + process.env.PORT,
     methods: ["GET", "POST"]
   }
 });
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,7 +7,7 @@ import io from 'socket.io-client'
 import { Navigation } from "./components";
 
 
-const socket = io.connect("http://0.0.0.0:3001")
+const socket = io.connect(process.env.NODE_ENV + ":3001")
 
 ReactDOM.render(
   <React.StrictMode>
