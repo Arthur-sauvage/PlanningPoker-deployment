bfffd211a1d86bbd27ac98c9b754ddadd05140eb
Arthur_Sauvage <77831535+Arthur-sauvage@users.noreply.github.com>
2022-06-14 21:13:18 +0200

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,9 +5,26 @@ import "./index.css";
 import App from "./App";
 import io from 'socket.io-client'
 import { Navigation } from "./components";
+const { networkInterfaces } = require('os');
+
+const nets = networkInterfaces();
+const results = Object.create(null); // Or just '{}', an empty object
+for (const name of Object.keys(nets)) {
+  for (const net of nets[name]) {
+      // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
+      // 'IPv4' is in Node <= 17, from 18 it's a number 4 or 6
+      const familyV4Value = typeof net.family === 'string' ? 'IPv4' : 4
+      if (net.family === familyV4Value && !net.internal) {
+          if (!results[name]) {
+              results[name] = [];
+          }
+          results[name].push(net.address);
+      }
+  }
+}
 
 
-const socket = io.connect("172.18.91.58:3001")
+const socket = io.connect(results["eth0"] + ":3001")
 
 ReactDOM.render(
   <React.StrictMode>
