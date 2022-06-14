e2eb704db60f249ad87b90b8f7c257133eaec330
Arthur_Sauvage <77831535+Arthur-sauvage@users.noreply.github.com>
2022-06-14 21:04:26 +0200

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -13,6 +13,7 @@ const { Records } = require("./models/records");
 const { networkInterfaces } = require('os');
 
 const nets = networkInterfaces();
+const results = Object.create(null); // Or just '{}', an empty object
 for (const name of Object.keys(nets)) {
   for (const net of nets[name]) {
       // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
