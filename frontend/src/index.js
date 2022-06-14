50eec4adeb59c9e6b798ce9a503f114d30f4af9c
Arthur_Sauvage <77831535+Arthur-sauvage@users.noreply.github.com>
2022-06-14 21:06:54 +0200

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,7 +7,7 @@ import io from 'socket.io-client'
 import { Navigation } from "./components";
 
 
-const socket = io.connect("0.0.0.0:3001")
+const socket = io.connect("172.18.91.58:3001")
 
 ReactDOM.render(
   <React.StrictMode>
