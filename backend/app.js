5ebc81bd6612858c197429a155492fde0569c6c3
Arthur_Sauvage <77831535+Arthur-sauvage@users.noreply.github.com>
2022-06-14 20:59:05 +0200

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -10,6 +10,23 @@ const { MongoClient } = require('mongodb');
 const connection = require("./db");
 const { User } = require("./models/user");
 const { Records } = require("./models/records");
+const { networkInterfaces } = require('os');
+
+const nets = networkInterfaces();
+for (const name of Object.keys(nets)) {
+  for (const net of nets[name]) {
+      // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
+      // 'IPv4' is in Node <= 17, from 18 it's a number 4 or 6
+      const familyV4Value = typeof net.family === 'string' ? 'IPv4' : 4
+      if (net.family === familyV4Value && !net.internal) {
+          if (!results[name]) {
+              results[name] = [];
+          }
+          results[name].push(net.address);
+      }
+  }
+
+  console.log(results)
 
 // server
 const server = http.createServer(app);
